290d567a73c686112306df6e1ba143222aba886d
Nikita-Apandov <apandovnikita@gmail.com>
2025-09-21 20:56:41 +0300

    network

--- terminal.py
+++ terminal.py
@@ -19,31 +19,31 @@ with open('configtxt/devices.txt', 'w') as f:
     """
     f.write(mc) 
     '''
-with open('configyaml/devices.yaml', 'w') as f: 
+with open('configyaml/dev_afto_new_pass.yaml', 'w') as f: 
     device =  [
         {
         "device_type": "asys",
         "host": "192.168.10.1",
         "username": "admin",
-        "password": "cisco",
+        "password": "12345678",
         "secret": "cisco",
-        "commands": ["show ip route", "show ip arp"]
+        "commands": ["configure terminal", "aaa new-model", "username admin secret cisco", "end", "write"]
     },
     {
         "device_type": "laim",
         "host": "192.168.10.2",
         "username": "admin",
-        "password": "cisco",
+        "password": "12345678",
         "secret": "cisco",
-        "commands": ["show ip route", "show ip arp"]
+        "commands": ["configure terminal", "aaa new-model", "username admin secret cisco", "end", "write"]
     },
     {
         "device_type": "star",
         "host": "192.168.10.3",
         "username": "admin",
-        "password": "cisco",
+        "password": "12345678",
         "secret": "cisco",
-        "commands": ["show ip route", "show ip arp"]
+        "commands": ["configure terminal", "aaa new-model", "username admin secret cisco", "end", "write"]
     }
     ]
     
--- test5.py
+++ test5.py
@@ -1,5 +1,5 @@
-# задание 5.1 (a, b, c, d)
-'''
+
+#5.1 (abcd)
 london_co = {
     "r1": {
         "location": "21 New Globe Walk",
@@ -25,93 +25,61 @@ london_co = {
         "routing": True
     }
 }
-deva = ', '.join(london_co.keys())
-device = input(f'Введите имя устройства({deva}): ')
-print(london_co.get(device, 'Такого устройства нет'))
-params = ', '.join(london_co[device].keys())
-parameter = input(f'Введите имя параметра ({params}): ')
-param_name = parameter.lower()
-print(london_co[device].get(param_name, 'Такого параметра нет'))
-'''
-"""
-#задание 5.2 (a)
-# Запрос ввода сети у пользователя
-ip_input = input("Введите IP-сеть в формате x.x.x.x/yy: ")
-
-# Разделяем IP и маску
-ip_part, mask_part = ip_input.split('/')
-ip_octets = list(map(int, ip_part.split('.')))
-mask_length = int(mask_part)
-
-# Вычисляем маску в десятичном и двоичном формате
-mask_bin = "1" * mask_length + "0" * (32 - mask_length)
-mask_octets = [int(mask_bin[i:i+8], 2) for i in range(0, 32, 8)]
-
-# Вывод информации о сети
-print("Network:")
-print(" ".join(str(octet) for octet in ip_octets))
-print(" ".join(f"{octet:08b}" for octet in ip_octets))
-print()
-print("Mask:")
-print(f"/{mask_length}")
-print(" ".join(str(octet) for octet in mask_octets))
-print(" ".join(mask_bin[i:i+8] for i in range(0, 32, 8)))
-
-
-
-#задание 5.3 
-
-switchort = input("Введите режим работы интерфейса (access/trunk): ") 
-interface = input("Введите тип и номер интерфейса: ")
-vlan = input("Введите номер влан(ов): ")
-print(access_template.get(switchort, 'Такого устройства нет'))
-
-access_template = [
-    "switchport mode access", "switchport access vlan {}",
-    "switchport nonegotiate", "spanning-tree portfast",
-    "spanning-tree bpduguard enable"
-]
-
-trunk_template = [
-    "switchport trunk encapsulation dot1q", "switchport mode trunk",
-    "switchport trunk allowed vlan {}"
-]
-
-"""
-
-########################################################################
-# 5.1 (abcd)
-london_co = {
-    "r1": {
-        "location": "21 New Globe Walk",
-        "vendor": "Cisco",
-        "model": "4451",
-        "ios": "15.4",
-        "ip": "10.255.0.1"
-    },
-    "r2": {
-        "location": "21 New Globe Walk",
-        "vendor": "Cisco",
-        "model": "4451",
-        "ios": "15.4",
-        "ip": "10.255.0.2"
-    },
-    "sw1": {
-        "location": "21 New Globe Walk",
-        "vendor": "Cisco",
-        "model": "3850",
-        "ios": "3.6.XE",
-        "ip": "10.255.0.101",
-        "vlans": "10,20,30",
-        "routing": True
-    }
-}
-devaiss = ', '.join(london_co.keys())
-sors = input(f"Введите имя устройства {devaiss}: ")
-deva = ', '.join(london_co[sors].keys())
-parametr = input(f"Введите имя параметра ({deva}): ")
-para = parametr.lower()
-out = london_co[sors].get(para, "Значения нет")
+devices = ', '.join(london_co.keys()) # собрали список ключей в одну строку с указанным разделителем 
+spicok = input(f'Введите имя устройства из списка {devices}: ')
+device = ', '.join(london_co[spicok].keys()) 
+parametr = input(f'Введите параметер устройства из списка {device}: ')
+param = parametr.lower() # преобразуем в нижний регистор
+out = london_co[spicok].get(param, 'Вы ввели на верный параметр') # если есть выводим значение, если нет выводим предупреждение
 print(out)
-#5.2 
-octet = input("Введите IP-сети в формате xxx.xxx.xxx.xxx/xx: ")
+
+#5.2 ( a) очень трудно я не понимаю)!
+network = input("Введите IP-сети в формате xxx.xxx.xxx.xxx/xx: ")
+new = network.split('/')
+octet = new[0].split('.')
+#octet = bin(int(new[0].replace('.', '')))
+mask = '1' * int(new[1]) + '0' * (32 - int(new[1])) # нельзя умножать строку на строку!
+
+mask_one = int(mask[0:8], 2)
+mask_two = int(mask[8:16], 2) # конвентируем из двоичной системы в десятичную
+mask_three = int(mask[16:24], 2)
+mask_four = int(mask[24:32], 2)
+
+print(f'''
+Network:
+{octet[0]:<10}{octet[1]:<10}{octet[2]:<10}{octet[3]:<10}
+{int(octet[0]):08b}  {int(octet[1]):08b}  {int(octet[2]):08b}  {int(octet[3]):08b}
+
+Mask:
+/{new[1]}
+{mask[0:8]:<10}{mask[8:16]:<10}{mask[16:24]:<10}{mask[24:32]:<10} 
+{mask_one:<10}{mask_two:<10}{mask_three:<10}{mask_four:<10}
+''')
+
+# 5.3 (a) это гениальный способ обойти циклы с помощью словарей!
+access_template = [
+"switchport mode access", "switchport access vlan {}",
+"switchport nonegotiate", "spanning-tree portfast",
+"spanning-tree bpduguard enable"
+]
+trunk_template = [
+"switchport trunk encapsulation dot1q", "switchport mode trunk",
+"switchport trunk allowed vlan {}"
+]
+
+template = {
+    'access': access_template,
+    'trunk': trunk_template
+}
+
+questions = {
+    'access': 'Введите номер VLAN: ',
+    'trunk': 'Введите разрешенные VLANы: '
+}
+
+mode = input("Введите режим работы интерфейса (access/trunk): ") 
+interface = input("Введите тип и номер интерфейса: ")
+vlan = input(questions[mode])
+print(f'interface: {interface}')
+print('\n'.join(template[mode]).format(vlan))
+   
