d3f5e175405c1c888b29b95ff65fbadeaa941b88
Nikita-Apandov <apandovnikita@gmail.com>
2025-10-13 14:12:12 +0300

    network

--- test9.py
+++ test9.py
@@ -1,4 +1,6 @@
 
+from pprint import pprint 
+
 #username = input('Введите имя пользователя: ')
 #password = input('Введите пароль: ')
 '''
@@ -77,89 +79,125 @@ def check_passwd(username, password, min_len=8, check_numbers=False):
     
 check_passwd('admin', 'adsfsdgsdfgsdf234234234')
 ###############################################
-'''
 
+# task 9.1 (a)
 
-'''
-# task 9.1 a
-
-from pprint import pprint 
-
-
-def generate_access_config(access_config, access_mode_template, port_security_template, port_security_temp=True):
+def config_access(access_mode, access_config, port_security, security=True):
     config = []
-    for intf, vlan in access_config.items():
-        config.append(f"{intf}")
-        for command in access_mode_template:
+    for intr, vlan in access_config.items():
+        config.append(f'{intr}')
+        for command in access_mode:
             if command.endswith("access vlan"):
-                config.append(f" {command} {vlan}")
-            else: 
-                config.append(f" {command}")
-        for com in port_security_template:
-            if port_security_temp is not False:
-                config.append(f" {com}")
+                config.append(f' {command} {vlan}')
+            else:
+                config.append(f' {command}')
+        
+        if security:
+            for command_security in port_security:
+                config.append(f' {command_security}')
     return config
 
-def main():
-    access_mode_template = [
-        "switchport mode access", "switchport access vlan",
-        "switchport nonegotiate", "spanning-tree portfast",
-        "spanning-tree bpduguard enable"
-    ]
-    access_config = {
-        "FastEthernet0/12": 10,
-        "FastEthernet0/14": 11,
-        "FastEthernet0/16": 17
-    }
-    port_security_template = [
-    "switchport port-security maximum 2",
-    "switchport port-security violation restrict",
-    "switchport port-security"
-    ]
 
-    result = generate_access_config(access_config, access_mode_template, port_security_template)
-    pprint(result)
+access_mode_template = [
+"switchport mode access", "switchport access vlan",
+"switchport nonegotiate", "spanning-tree portfast",
+"spanning-tree bpduguard enable"
+]
+access_config = {
+"FastEthernet0/12": 10,
+"FastEthernet0/14": 11,
+"FastEthernet0/16": 17
+}
 
-main()
-
-
-#task 9.2 a
-
-from pprint import pprint 
-
-
-def generate_access_config(trunk_config, trunk_mode_template):
-    config_dict = {}
-    
-    for intf, vlans in trunk_config.items():
-        config = []
-        config.append(f"{intf}")
-        for command in trunk_mode_template:
-            if command.endswith("allowed vlan"):
-                vlan_str = ','.join(str(vlan) for vlan in vlans)
-                config.append(f" {command} {vlan_str}")
-
-            else: 
-                config.append(f" {command}")
-        config_dict[intf] = config
-    return config_dict
-
-def main():
-    trunk_mode_template = [
-    "switchport mode trunk", "switchport trunk native vlan 999",
-    "switchport trunk allowed vlan"
+port_security_template = [
+"switchport port-security maximum 2",
+"switchport port-security violation restrict",
+"switchport port-security"
 ]
 
-    trunk_config = {
-    "FastEthernet0/1": [10, 20, 30],
-    "FastEthernet0/2": [11, 30],
-    "FastEthernet0/4": [17]
-}
-  
+result = config_access(access_mode_template, access_config, port_security_template)
+pprint(result)
 
-    result = generate_access_config(trunk_config, trunk_mode_template)
+
+# tack 9.2 
+
+def generate_trunk_config(trunk_mode, trank_config):
+    config_dict = {}
+    for inter, vlans in trank_config.items(): 
+        config = []
+        
+        for trunk in trunk_mode:        
+            if trunk.endswith('allowed vlan'):
+                vlan_str = ','.join(str(vlan) for vlan in vlans)
+                config.append(f' {trunk} {vlan_str}')
+            else:
+                config.append(f' {trunk}')
+        config_dict[inter] = config
+    return config_dict
+
+trunk_mode_template = [
+"switchport mode trunk", "switchport trunk native vlan 999",
+"switchport trunk allowed vlan"
+]
+
+trunk_config = {
+"FastEthernet0/1": [10, 20, 30],
+"FastEthernet0/2": [11, 30],
+"FastEthernet0/4": [17]
+}
+
+result = generate_trunk_config(trunk_mode_template, trunk_config)
+pprint(result)
+
+
+# tack 9.3 (a не было конфига для обработки)
+
+def get_int_vlan_map(config):
+    config_dict = {}
+    interface = ''
+    for line in config: 
+        if 'interface gigabitEthernet ' in line:
+            interface = line.strip()
+        elif 'access vlan' in line:
+            line = line.split()
+            vlan = line[-1]
+            config_dict[interface] = vlan
+        elif 'allowed vlan' in line:
+            line = line.split(' ')
+            vlans = line[-1].split(',')
+            vlan_str = ', '.join(str(vlan) for vlan in vlans)
+            config_dict[interface] = vlan_str.strip()
+    return config_dict
+
+with open('configtxt/comutator.txt') as config:     
+    result = get_int_vlan_map(config)
     pprint(result)
 
-main()
 '''
-# task 9.3 
+
+# tack 9.4 (что бы понять нужно немного подумать)
+
+def convert_config_to_dict(config):
+    config_dict = {}
+    interface = ""
+    configure = [] # вынесли из цикла 
+    for line in config: 
+        if 'interface Ethernet' in line:
+            # сохраняем предыдущий интерфейс перед началом нового 
+            if interface and configure:
+                config_dict[interface] = configure
+            # начинаем новый интерфейс 
+            interface = line.strip()
+            configure = [] # новый список для нового интерфейса 
+        elif interface: # добавляем только если есть активный интерфейс 
+            if 'switchport' in line:
+                configure.append(line)
+            elif 'duplex auto' in line:
+                configure.append(line)
+            elif 'spanning-tree' in line:
+                configure.append(line)
+    return config_dict
+
+with open('configtxt/config_sw1.txt') as config_filename: 
+    result = convert_config_to_dict(config_filename)
+    pprint(result)
